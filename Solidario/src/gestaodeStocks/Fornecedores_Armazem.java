f580bdec528c14db56738a4e0999601257473b93
HugoCosta <hugobicacosta@gmail.com>
2017-01-26 20:22:18 +0000

    correção

--- Solidario/src/gestaodeStocks/Fornecedores_Armazem.java
+++ Solidario/src/gestaodeStocks/Fornecedores_Armazem.java
@@ -2,37 +2,44 @@ package gestaodeStocks;
 
 public class Fornecedores_Armazem {
 
+	public Entrada_Armazem Entrada_Armazem_Master;
+	public int cod_tipo_fornecedor;
+	public int cod_fornecedor;
+	
 	public Fornecedores_Armazem() {
 		// TODO Auto-generated constructor stub
 	
 	
 	
 	}
-	public int cod_fornecedor_armazem;
-	public int cod_tipo_fornecedor;
-	public int cod_fornecedor;
-	
-	
-	public int getCod_fornecedor_armazem() {
-		return cod_fornecedor_armazem;
+
+	public Entrada_Armazem getEntrada_Armazem_Master() {
+		return Entrada_Armazem_Master;
 	}
+
+	public void setEntrada_Armazem_Master(Entrada_Armazem entrada_Armazem_Master) {
+		Entrada_Armazem_Master = entrada_Armazem_Master;
+	}
+
 	public int getCod_tipo_fornecedor() {
 		return cod_tipo_fornecedor;
 	}
-	public int getCod_fornecedor() {
-		return cod_fornecedor;
-	}
-	
 
-	public void setCod_fornecedor_armazem(int cod_fornecedor_armazem) {
-		this.cod_fornecedor_armazem = cod_fornecedor_armazem;
-	}
 	public void setCod_tipo_fornecedor(int cod_tipo_fornecedor) {
 		this.cod_tipo_fornecedor = cod_tipo_fornecedor;
 	}
+
+	public int getCod_fornecedor() {
+		return cod_fornecedor;
+	}
+
 	public void setCod_fornecedor(int cod_fornecedor) {
 		this.cod_fornecedor = cod_fornecedor;
 	}
+	
+	
+	
+	
 	
 	
 }
--- Solidario/src/gestaodeStocks/Tipo_Fornecedor_Armazem.java
+++ Solidario/src/gestaodeStocks/Tipo_Fornecedor_Armazem.java
@@ -1,30 +1,33 @@
 package gestaodeStocks;
 
 public class Tipo_Fornecedor_Armazem {
+	
+	public Fornecedores_Armazem Fornecedores_Armazem_Master;
+	public String nome_tipo_fornecedor_armazem;
+	
 	public Tipo_Fornecedor_Armazem() {
 		// TODO Auto-generated constructor stub
 	
 	
 	
 	}
-	public int cod_tipo_fornecedor;
-	public char nome_tipo_fornecedor_armazem;
 
-	
-	
-	public int getCod_tipo_fornecedor() {
-		return cod_tipo_fornecedor;
+	public Fornecedores_Armazem getFornecedores_Armazem_Master() {
+		return Fornecedores_Armazem_Master;
 	}
-	public char getNome_tipo_fornecedor_armazem() {
+
+	public void setFornecedores_Armazem_Master(Fornecedores_Armazem fornecedores_Armazem_Master) {
+		Fornecedores_Armazem_Master = fornecedores_Armazem_Master;
+	}
+
+	public String getNome_tipo_fornecedor_armazem() {
 		return nome_tipo_fornecedor_armazem;
 	}
-	
 
-	public void setCod_tipo_fornecedor(int cod_tipo_fornecedor) {
-		this.cod_tipo_fornecedor = cod_tipo_fornecedor;
-	}
-	public void setNome_tipo_fornecedor_armazem(char nome_tipo_fornecedor_armazem) {
+	public void setNome_tipo_fornecedor_armazem(String nome_tipo_fornecedor_armazem) {
 		this.nome_tipo_fornecedor_armazem = nome_tipo_fornecedor_armazem;
 	}
+	
+
 	
 }
